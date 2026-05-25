56154eecc7c8bf3ff51177add0d2c30f91c47067
Martin Mazas <martinmazas@Martins-MacBook-Pro.local>
2026-05-25 23:31:32 +0300

    login test with credentials from registeredUser

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -10,8 +10,9 @@ test.use({ storageState: { cookies: [], origins: [] } });
 test.describe('Authentication', () => {
   test('Login > valid credentials land on account overview', async ({
     loginPage,
+    registeredUser
   }) => {
-    await loginPage.login(process.env.TEST_USERNAME!, process.env.TEST_PASSWORD!);
+    await loginPage.login(registeredUser.username, registeredUser.password);
     await loginPage.expectOverview();
   });
 
